c41c6327887b0eb132d51a54834bce8b1aa7485e
GersonEC <gersonenriquez94@gmail.com>
2024-09-20 18:10:25 +0200

    test getErrorMessage console error spy

--- exercises/04.unit-test/02.problem.spies/app/utils/misc.error-message.test.ts
+++ exercises/04.unit-test/02.problem.spies/app/utils/misc.error-message.test.ts
@@ -1,6 +1,6 @@
 import { faker } from '@faker-js/faker'
 // 🐨 you'll get vi from here
-import { expect, test } from 'vitest'
+import { expect, test, vi } from 'vitest'
 import { getErrorMessage } from './misc.tsx'
 
 test('Error object returns message', () => {
@@ -16,8 +16,16 @@ test('String returns itself', () => {
 test('undefined falls back to Unknown', () => {
 	// 🐨 use spyOn on the console's error property
 	// 🐨 mock the implementation with a function that does nothing (💰 () => {})
+	const consoleError = vi.spyOn(console, 'error')
+	consoleError.mockImplementation(() => {})
 	expect(getErrorMessage(undefined)).toBe('Unknown Error')
 	// 🐨 make sure console.error was once
+	expect(consoleError).toHaveBeenCalledOnce()
 	// 🐨 assert that it was called with the right arguments
+	expect(consoleError).toHaveBeenCalledWith(
+		'Unable to get error message for error',
+		undefined,
+	)
 	// 🐨 restore the mock so we don't swallow errors for other tests.
+	consoleError.mockRestore()
 })
